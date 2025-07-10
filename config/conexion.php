477efbbd0ddba444abbfac8375a8056ab943ca3e
Harold Yataco <133307182+har02old2003@users.noreply.github.com>
2025-07-10 11:18:51 -0500

    Fix: proteger declaración de clase Conectar para evitar error de clase duplicada

--- config/conexion.php
+++ config/conexion.php
@@ -1,42 +1,44 @@
 <?php
 
 /* TODO: Inicio de Session */
-class Conectar {
-    protected $dbh;
-    
-    protected function Conexion() {
-        try {
-            // TODO: Cadena de Conexión para Azure MySQL
-            $host = $_ENV['DB_HOST'] ?? 'databasefarma.mysql.database.azure.com';
-            $port = $_ENV['DB_PORT'] ?? '3306';
-            $database = $_ENV['DB_DATABASE'] ?? 'mysampledb';
-            $username = $_ENV['DB_USERNAME'] ?? 'administrador';
-            $password = $_ENV['DB_PASSWORD'] ?? '';
-            
-            // Configuración específica para Azure MySQL
-            $options = [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::MYSQL_ATTR_SSL_CA => false, // Azure requiere SSL
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
-            ];
-            
-            $conectar = $this->dbh = new PDO("mysql:host={$host};port={$port};dbname={$database};charset=utf8mb4", $username, $password, $options);
-            return $conectar;
-        } catch (Exception $e) {
-            /* TODO: En caso de error mostrar mensaje */
-            print "Error Conexion BD: " . $e->getMessage() . "<br/>";
-            die();
+if (!class_exists('Conectar')) {
+    class Conectar {
+        protected $dbh;
+        
+        protected function Conexion() {
+            try {
+                // TODO: Cadena de Conexión para Azure MySQL
+                $host = $_ENV['DB_HOST'] ?? 'databasefarma.mysql.database.azure.com';
+                $port = $_ENV['DB_PORT'] ?? '3306';
+                $database = $_ENV['DB_DATABASE'] ?? 'mysampledb';
+                $username = $_ENV['DB_USERNAME'] ?? 'administrador';
+                $password = $_ENV['DB_PASSWORD'] ?? '';
+                
+                // Configuración específica para Azure MySQL
+                $options = [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::MYSQL_ATTR_SSL_CA => false, // Azure requiere SSL
+                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
+                ];
+                
+                $conectar = $this->dbh = new PDO("mysql:host={$host};port={$port};dbname={$database};charset=utf8mb4", $username, $password, $options);
+                return $conectar;
+            } catch (Exception $e) {
+                /* TODO: En caso de error mostrar mensaje */
+                print "Error Conexion BD: " . $e->getMessage() . "<br/>";
+                die();
+            }
         }
-    }
-    
-    public static function ruta() {
-        $app_url = $_ENV['APP_URL'] ?? 'http://localhost';
-        return $app_url . "/";
-    }
-    
-    public static function ruta_base_menu() {
-        $app_url = $_ENV['APP_URL'] ?? 'http://localhost';
-        return $app_url . "/";
+        
+        public static function ruta() {
+            $app_url = $_ENV['APP_URL'] ?? 'http://localhost';
+            return $app_url . "/";
+        }
+        
+        public static function ruta_base_menu() {
+            $app_url = $_ENV['APP_URL'] ?? 'http://localhost';
+            return $app_url . "/";
+        }
     }
 } 
